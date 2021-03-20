b401a2da99d3b579bd3dae0d5269c37dc228573f
maxly20 <maxly2011@gmail.com>
2021-03-20 13:24:23 +1100

    refactor Header css

--- src/Header.js
+++ src/Header.js
@@ -1,22 +1,21 @@
-import React from "react";
-import "./Header.css";
-import SearchIcon from "@material-ui/icons/Search";
-import LanguageIcon from "@material-ui/icons/Language";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import { Avatar } from "@material-ui/core";
-import { Link } from "react-router-dom";
+import React from 'react';
+import SearchIcon from '@material-ui/icons/Search';
+import LanguageIcon from '@material-ui/icons/Language';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { Avatar } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <div className="header">
-      <Link to="/" style={{ textDecoration: 'none' }}>
+    <div className='header'>
+      <Link to='/' style={{ textDecoration: 'none' }}>
         <h2 className='header__logo'>FlatsMeet</h2>
       </Link>
-      <div className="header__center">
-        <input type="text" />
+      <div className='header__center'>
+        <input type='text' />
         <SearchIcon />
       </div>
-      <div className="header__right">
+      <div className='header__right'>
         <p>Become a host</p>
         <LanguageIcon />
         <ExpandMoreIcon />
