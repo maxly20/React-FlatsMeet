221949ee54a7fa50cf04149add5cb2d4413c5ce6
maxly20 <maxly2011@gmail.com>
2021-03-21 08:28:48 +1100

    restructure Footer css

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <div className='footer'>
-      <p>© 2020 Airbnb clone! No rights reserved - this is a demo!</p>
+      <p>© 2021 Flatsmeet! No rights reserved - this is a demo!</p>
       <p>Privacy · Terms · Sitemap · Company Details</p>
     </div>
   );
