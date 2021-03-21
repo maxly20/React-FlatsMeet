767af1dc20d8221ce401499bd506f8572090929d
maxly20 <maxly2011@gmail.com>
2021-03-22 10:09:32 +1100

    rebuild file structure header.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Home from './Home';
-import Header from './Header';
+import Header from './Components/Header';
 import Footer from './Footer';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchPage from './SearchPage';
