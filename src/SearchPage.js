c51b0b8f6ba8397e29539218288f4eb9fe151a1f
maxly20 <maxly2011@gmail.com>
2021-03-22 10:14:22 +1100

    rebuild file structure SearchResult.js

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Button } from '@material-ui/core';
-import SearchResult from './SearchResult';
+import SearchResult from './Components/SearchResult';
 
 function SearchPage() {
   return (
